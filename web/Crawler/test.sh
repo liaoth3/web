aa84f323373139ea483af3e7dded1316fd9c7ab0
liaoth31 <liaoth31@163.com>
2015-11-17 09:28:28 +0800

    sth

--- web/Crawler/test.sh
+++ web/Crawler/test.sh
@@ -1,8 +1,13 @@
 clear
 while true
 do
-	php ./run.php 0 10 1
-	sleep 1
+	/etc/init.d/php5-fpm restart
+	/etc/init.d/nginx restart
+	for((i=0;i<2000;i++))
+	do
+		php ./run.php 0 10 1
+		sleep 1
+	done
 done
 
 
--- web/Crawler/test2.sh
+++ web/Crawler/test2.sh
@@ -1,8 +1,13 @@
 clear
 while true
 do
-	php ./run.php 10 20 1
-	sleep 1
+	/etc/init.d/php5-fpm restart
+	/etc/init.d/nginx restart
+	for((i=0;i<2000;i++))
+	do
+		php ./run.php 10 20 1
+		sleep 1
+	done
 done
 
 
